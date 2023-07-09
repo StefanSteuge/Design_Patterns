29689bb0a9dc1a17ff70cf415adce9153c707bfc
StefanSteuge <fotosteuge@gmail.com>
2023-07-09 14:43:06 +0300

    added new classes and Order example to Builder branch

--- src/Order/Main.java
+++ src/Order/Main.java
@@ -1,4 +1,23 @@
 package Order;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+
+      Order order = new Order.Builder("first", "Steven")
+              .phoneNumber(1560987)
+              .items(Arrays.asList("item1", "item2", "item3"))
+              .shippingAddress("Lviv 79053")
+              .isPaid(true)
+              .build();
+        System.out.println("Welcome to Order" + order.toString());
+
+      Order secondOrder = new Order.Builder("second", "Eugene")
+              .isPaid(false)
+              .build();
+        System.out.println("Welcome to Order" + secondOrder);
+    }
+
 }
--- src/Order/Order.java
+++ src/Order/Order.java
@@ -1,4 +1,98 @@
 package Order;
 
+import java.util.List;
+
 public class Order {
+
+    private String orderId;
+    private String client;
+    private String shippingAddress;
+    private int phoneNumber;
+    private List<String> items;
+    private boolean isPaid;
+
+    public Order(Builder builder) {
+        this.orderId = builder.orderId;
+        this.client = builder.client;
+        this.shippingAddress = builder.shippingAddress;
+        this.phoneNumber = builder.phoneNumber;
+        this.items = builder.items;
+        this.isPaid = builder.isPaid;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public String getClient() {
+        return client;
+    }
+
+    public String getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public List<String> getItems() {
+        return items;
+    }
+
+    public boolean isPaid() {
+        return isPaid;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId='" + orderId + '\'' +
+                ", client='" + client + '\'' +
+                ", shippingAddress='" + shippingAddress + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", items=" + items +
+                ", isPaid=" + isPaid +
+                '}';
+    }
+
+    public static class Builder {
+
+        private String orderId;
+        private String client;
+        private String shippingAddress;
+        private int phoneNumber;
+        private List<String> items;
+        private boolean isPaid;
+
+        public Builder(String orderId, String client) {
+            this.orderId = orderId;
+            this.client = client;
+        }
+
+        public Builder shippingAddress(String shippingAddress) {
+            this.shippingAddress = shippingAddress;
+            return this;
+        }
+
+        public Builder phoneNumber(int phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public Builder items(List<String> items) {
+            this.items = items;
+            return this;
+        }
+
+        public Builder isPaid(boolean isPaid) {
+            this.isPaid = isPaid;
+            return this;
+        }
+
+        public Order build() {
+            return new Order(this);
+        }
+    }
+
 }
